da75cc32f2e318948a317ec1c1defabe9868fd06
phi <caubesongngu8@gmail.com>
2026-07-03 06:51:02 +0700

    fix: add production domain to CORS whitelist

--- eiems-backend/src/main.ts
+++ eiems-backend/src/main.ts
@@ -3,13 +3,16 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   app.enableCors({
-    origin: 'http://localhost:3001 , https://bysms-bakary-management-sytems.vercel.app/login',
+    origin: [
+      'http://localhost:3001',
+      'https://bysms-bakary-management-sytems.vercel.app',
+    ],
     credentials: true,
   });
 
   await app.listen(process.env.PORT || 3000);
 }
-
 
 void bootstrap();
