c203a5fbe42c660f91f8d37cc3655efde89d8532
phi <caubesongngu8@gmail.com>
2026-07-03 06:14:18 +0700

    fix Dockerfile

--- eiems-backend/dist/src/main.js
+++ eiems-backend/dist/src/main.js
@@ -8,7 +8,7 @@ async function bootstrap() {
         origin: 'http://localhost:3001',
         credentials: true,
     });
-    await app.listen(3000);
+    await app.listen(process.env.PORT || 3000);
 }
 void bootstrap();
 //# sourceMappingURL=main.js.map
